0177df5badd01218962033f594859c9b38d0e44a
Fazle-Rakib <fazle30@student.sust.edu>
2023-10-14 00:08:36 +0600

    Play around game loop with time calculation

---
+++ GameLoop/main.cpp
@@ -0,0 +1,128 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+#define SCREEN_WIDTH 1080
+#define SCREEN_HEIGHT 520
+
+// Global variables
+bool gameIsRunning = false;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+int redColorCode = 0;
+Uint32 startTime;
+Uint32 currentTime;
+
+bool initializeWindow(void)
+{
+    // Initialize SDL with video support
+    // Automatically initializes the Event Handling, File I/O and Threading subsystems
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
+        return false;
+    }
+
+    // Create an SDL window
+    window = SDL_CreateWindow(
+        "SDL Introduction",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        0);
+
+    if (!window)
+    {
+        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
+        return false;
+    }
+
+    // Create an SDL renderer for rendering graphics in the window
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+void process_input(void)
+{
+    // Poll SDL events (e.g., window close)
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            gameIsRunning = false; // Exit the game loop
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+void incrementVariable()
+{
+    // increase redColorCode & reset after max 8 bit value
+    redColorCode = redColorCode + 25;
+    if (redColorCode >= 256)
+    {
+        redColorCode = 0;
+    }
+}
+
+void update(void)
+{
+    // Set the render draw color (R, G, B, A)
+    SDL_SetRenderDrawColor(renderer, redColorCode, 20, 205, 255);
+
+    // Clear the renderer with the specified draw color
+    SDL_RenderClear(renderer);
+
+    // Present the renderer (draw the frame to the window)
+    SDL_RenderPresent(renderer);
+
+    // Calculate time elapsed since the start
+    currentTime = SDL_GetTicks();
+    Uint32 elapsedTime = currentTime - startTime;
+
+    // Check if 0.5 seconds (500 milliseconds) have passed
+    if (elapsedTime >= 500)
+    {
+        incrementVariable();
+        startTime = currentTime;
+    }
+}
+
+void destroyWindow(void)
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char **argv)
+{
+    // The game loop control variable
+    gameIsRunning = initializeWindow();
+    startTime = SDL_GetTicks();
+
+    // Game loop: keep the application running until 'running' is set to false
+    while (gameIsRunning)
+    {
+        // Continuously polls for SDL events
+        process_input();
+
+        // Where the magic happens
+        update();
+    }
+
+    // Clean up and exit the application
+    destroyWindow();
+
+    return 0;
+}
